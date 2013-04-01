88d6cef94165756ecb0575f37ef359d441ed9f9b
qbright <502268502@qq.com>
2013-04-01 13:16:00 +0800

    test
    Signed-off-by: qbright <502268502@qq.com>

--- lpms/lpms_server/src/test/java/me/qbright/sigar/test/SigarTest.java
+++ lpms/lpms_server/src/test/java/me/qbright/sigar/test/SigarTest.java
@@ -1,11 +1,17 @@
 package me.qbright.sigar.test;
 
+import org.hyperic.sigar.CpuInfo;
+import org.hyperic.sigar.Sigar;
+import org.hyperic.sigar.SigarException;
+import org.junit.Test;
+
 
 public class SigarTest {
-/*	public void BasciTest() throws SigarException{
+	@Test
+	public void BasciTest() throws SigarException {
 		System.out.println(System.getProperty("user.dir"));
-		System.setProperty("java.library.path", "src/main/resources/");
+		System.setProperty("java.library.path", "src/main/resources/sigar-lib/");
 		Sigar sigar = new Sigar();
 		CpuInfo[] cpuInfos =  sigar.getCpuInfoList();
-	}*/
+	}
 }
