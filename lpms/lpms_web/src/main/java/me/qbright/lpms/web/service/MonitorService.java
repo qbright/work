49749fffb69d593202e9793383263c07093b056f
qbright <502268502@qq.com>
2013-05-07 13:10:55 +0800

    Signed-off-by: qbright <502268502@qq.com> 添加了各个请求

--- lpms/lpms_web/src/main/java/me/qbright/lpms/web/service/MonitorService.java
+++ lpms/lpms_web/src/main/java/me/qbright/lpms/web/service/MonitorService.java
@@ -5,6 +5,7 @@
  */
 package me.qbright.lpms.web.service;
 
+import java.util.List;
 import java.util.Map;
 
 import me.qbright.lpms.web.entity.ServerMachine;
@@ -16,4 +17,12 @@ import org.springframework.stereotype.Service;
 @Repository
 public interface MonitorService {
 	Map<String, Object> getGeneralInfo(ServerMachine serverMachine);
+	
+	Map<String,Object> getJavaEnvironment(ServerMachine serverMachine);
+	
+	List<Map<String, Object>> getFileSystemInfo(ServerMachine serverMachine);
+	
+	List<Map<String, Object>> getNetInfo(ServerMachine serverMachine);
+	
+	List<Map<String, Object>> getProcInfo(ServerMachine serverMachine);
 }
--- lpms/lpms_web/src/main/java/me/qbright/lpms/web/serviceimpl/MonitorServiceImpl.java
+++ lpms/lpms_web/src/main/java/me/qbright/lpms/web/serviceimpl/MonitorServiceImpl.java
@@ -5,27 +5,43 @@
  */
 package me.qbright.lpms.web.serviceimpl;
 
+import java.util.List;
 import java.util.Map;
 
-import me.qbright.lpms.web.dao.ServerMachineDao;
 import me.qbright.lpms.web.entity.ServerMachine;
 import me.qbright.lpms.web.monitor.MonitorUtil;
 import me.qbright.lpms.web.restclient.RestClient;
 import me.qbright.lpms.web.service.MonitorService;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component("monitorService")
 public class MonitorServiceImpl implements MonitorService {
-	
-	@Autowired
-	private ServerMachineDao serverMachineDao;
-	
+
+
 	@Override
 	public Map<String, Object> getGeneralInfo(ServerMachine serverMachine) {
-		return RestClient.getRestResponseAsMap(MonitorUtil.GENERAL_INFO,
-				serverMachine);
+		return RestClient.getRestResponseAsMap(MonitorUtil.GENERAL_INFO, serverMachine);
+	}
+
+	@Override
+	public Map<String, Object> getJavaEnvironment(ServerMachine serverMachine) {
+		return RestClient.getRestResponseAsMap(MonitorUtil.JAVA_ENVIRONMENT, serverMachine);
+	}
+
+	@Override
+	public List<Map<String, Object>> getFileSystemInfo(ServerMachine serverMachine) {
+		return RestClient.getRestResponseAsList(MonitorUtil.FILESYSTEM_INFO, serverMachine, "fileSystem_list");
+	}
+
+	@Override
+	public List<Map<String, Object>> getNetInfo(ServerMachine serverMachine) {
+		return RestClient.getRestResponseAsList(MonitorUtil.NET_INFO, serverMachine, "netInfo_list");
+	}
+
+	@Override
+	public List<Map<String, Object>> getProcInfo(ServerMachine serverMachine) {
+		return RestClient.getRestResponseAsList(MonitorUtil.PROC_INFO, serverMachine, "procInfo_list");
 	}
 
 }
